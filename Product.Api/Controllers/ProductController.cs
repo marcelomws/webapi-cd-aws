df1711694fb1c3181a676d3ffa81f9ab3be96eb9
Marcelo Wildfaier <mwildfaier@gmail.com>
2021-02-17 23:58:37 -0300

    feat: :tada: Adicionada WebApi de Produto

---
+++ Product.Api/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Product.Api.Models;
+
+namespace Product.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = new ProductModel[3]
+            {
+                new ProductModel { Id = 1, Description = "Product 01", Price = 10.0M },
+                new ProductModel { Id = 2, Description = "Product 02", Price = 15.0M },
+                new ProductModel { Id = 3, Description = "Product 03", Price = 5.50M }
+            };
+
+            return Ok(products);
+        }
+    }
+}
---
+++ Product.Api/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Product.Api.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
